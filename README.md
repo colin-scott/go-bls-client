8f52d70f7203e8c750f0d8905ae9cdd92464bc18
colin-scott <cs@cs.berkeley.edu>
2014-04-28 12:19:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+You will need to clone https://github.com/cridenour/go-bls and run `go install github.com/cridenour/go-bls`. I may have cut off the cridenour prefix, fwiw.
+
+Then to run, `go build`, and `./bls -series <SERIES>`
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "sort"
+    "github.com/go-bls"
+)
+
+func perror(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+func min(i, j int) int {
+  // the math package doesn't support ints?
+  if i < j {
+    return i
+  } else {
+    return j
+  }
+}
+
+func aggregate_all_years(series string, start_year, end_year int) *bls.TimeSeriesResults {
+    // BLS only allows 10 years per query. Query for each interval and squoosh
+    // the results into one struct.
+    // Pre: start_year < end_year
+    data := new(bls.TimeSeriesResults)
+    for left_bound := start_year; left_bound < end_year; left_bound += 10 {
+      right_bound := min(left_bound + 10, end_year)
+      params := bls.TimeSeriesParams{[]string{series}, left_bound, right_bound}
+      response := query(params)
+      if len((*data).Series) == 0 {
+        (*data).Series = response.Series
+      } else {
+        (*data).Series[0].Data = append((*data).Series[0].Data, response.Series[0].Data...)
+      }
+    }
+    sort.Sort((*data).Series[0].Data)
+    return data
+}
+
+func query(params bls.TimeSeriesParams) bls.TimeSeriesResults {
+    // TODO(cs): shouldn't the go-bls library return a reference to a
+    // TimeSeriesResults, not the value?
+    data, err := bls.GetSeries(params)
+    perror(err)
+
+    if len(data.Message) > 0 {
+        for _, message := range data.Message {
+            fmt.Printf("Error: %s\n", message)
+        }
+        panic("There were problems with your query")
+    }
+    return data.Results
+}
+
+func main() {
+    // -- Series format --
+    // prefix: CE (National employment stats)
+    // Seasonal Adjustment Code: S (Seasonal)
+    // Industry code: 50 (Information)
+    // Subfield code: 000000 (wildcard)
+    // data_type: 01 (total employees, in thousands)
+    // 'CES5000000001', 'CES4142343001'
+    series_label := flag.String("series", "CES5000000001", "BLS series to query. See http://www.bls.gov/help/hlpforma.htm#CE")
+    start_year := flag.Int("start_year", 1960, "Start year")
+    end_year := flag.Int("end_year", 2014, "End year")
+    flag.Parse()
+
+    if *start_year >= *end_year {
+      panic("Start year must come before end year")
+    }
+
+    aggregated := aggregate_all_years(*series_label, *start_year, *end_year)
+
+    for _, series := range (*aggregated).Series {
+        fmt.Printf("SeriesID: %s\n", series.SeriesID)
+        for _, data := range series.Data {
+            fmt.Printf("[%s %s] %s\n", data.Year, data.PeriodName, data.Value)
+        }
+        fmt.Printf("\n")
+    }
+}
